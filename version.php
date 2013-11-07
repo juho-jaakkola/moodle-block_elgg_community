2ddc0346388434ae333ec4ac6ab88a84d8243a39
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-11-07 15:20:43 +0200

    Added correct component name.

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'block_sms';
+$plugin->component = 'block_elgg_community';
 $plugin->version = 2012040515;
 $plugin->requires = 2010112400;
